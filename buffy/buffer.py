5bd73501829f96b3e2801f24961c7f32410065eb
bramwiggers <bramwiggers94@gmail.com>
2018-08-03 20:41:29 +0300

    Add Buffer

---
+++ buffy/buffer.py
@@ -0,0 +1,14 @@
+class Buffer (object):
+    def __init__(self, ph_start):
+        self.pH = ph_start
+
+    def add_drip(self, base_or_acid):
+        if base_or_acid == "base":
+            self.pH += 1
+        elif base_or_acid == "acid":
+            self.pH -= 1
+        else:
+            print("ERROR")
+
+    def read_ph(self):
+        return self.pH
