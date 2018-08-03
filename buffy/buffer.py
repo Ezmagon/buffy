69f3f7ce05250a1ccade5897c2ff8324abb23346
bramwiggers <bramwiggers94@gmail.com>
2018-08-03 23:22:36 +0300

    Add Buffer

--- buffy/buffer.py
+++ buffy/buffer.py
@@ -1,14 +1,40 @@
+import math
+
 class Buffer():
-    def __init__(self, ph_start):
-        self.pH = ph_start
+    def __init__(self, pKa=0, Amin=-1, HA=1, pipet_size=0.1):
+        """
+        :param pKa:
+        :param Amin:
+        :param HA:
+        """
+        self.pKa = pKa
+        self.Amin = Amin
+        self.HA = HA
+        self.pipet_size = pipet_size
 
     def add_drip(self, base_or_acid):
+        """
+        Changes the pH based on a drip of base or acid
+        :param base_or_acid:
+        :return:
+        """
         if base_or_acid == "base":
-            self.pH += 1
+            self.Amin += self.pipet_size
         elif base_or_acid == "acid":
-            self.pH -= 1
+            self.HA += self.pipet_size
         else:
             print("ERROR")
 
+    def calculate_pH(self):
+        """
+        :return: pKa + log10(Amin/HA)
+        """
+        return math.pow(self.Amin, 3)
+        # return self.pKa + math.log10(self.Amin / self.HA)
+
     def read_ph(self):
-        return self.pH
+        """
+        Read the pH value
+        :return: pH value
+        """
+        return self.calculate_pH()
