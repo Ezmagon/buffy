416bf94a2eafb4cb5039dfaf0e5b0aa80bdee4e5
Matthijs Tadema <M.J.Tadema@gmail.com>
2018-08-03 12:20:40 +0200

    Added graph, changed main to function that returns data

--- testSimulation/main.py
+++ testSimulation/main.py
@@ -2,19 +2,30 @@ from simulation import Simulation
 from drip import Drip
 from random import *
 
-pH_goal = 10
-s = Simulation(3)
-while s.pH != pH_goal:
-    print(s.pH)
-    size = randint(1, 10)
-    size /= 10
-    size = round(size, 1)
-    if s.pH < pH_goal:
-        bOrA = "base"
-    else:
-        bOrA = "acid"
-    d = Drip(size, bOrA)
-    s.add_drip(d)
-print(s.pH)
-if s.pH == pH_goal:
+
+def test(pH_goal = 10):
+    s = Simulation(3)
+    out = []
+    t = 0
+    out.append((t, s.pH))
+    while s.pH != pH_goal:
+
+        size = randint(1, 10)
+        size /= 10
+        size = round(size, 1)
+        if s.pH < pH_goal:
+            bOrA = "base"
+            t += size
+        else:
+            bOrA = "acid"
+            t -= size
+        d = Drip(size, bOrA)
+        s.add_drip(d)
+        out.append((t, s.pH))
+        print(s.pH)
+    #print(s.pH)
+    # This always happens
+    #if s.pH == pH_goal:
     print ("Goal Achieved!")
+    return out
+
