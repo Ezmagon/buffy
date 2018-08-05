6e8b123676a515173f0c7ef5777cf8c49e5577ce
Matthijs Tadema <M.J.Tadema@gmail.com>
2018-08-05 13:23:37 +0200

    Added IPython dependency

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-    install_requires =['numpy', 'tk-tools', 'imutils', 'matplotlib', 'opencv-python'],
+    install_requires =['numpy', 'tk-tools', 'imutils', 'matplotlib', 'opencv-python', 'IPython'],
     scripts = [
         "bin/buffy_run.py"
     ]
