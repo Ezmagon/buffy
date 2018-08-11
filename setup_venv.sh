02abd5d6a0c1503ee90c885575b3f397800ca73b
Matthijs <m.j.tadema@gmail.com>
2018-08-11 18:19:40 +0200

    Added script to setup virtual environment

---
+++ setup_venv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Setup virtualenvironment on unix system
+# With correct dependencies as described by the package
+# Updates dependencies if venv already exists
+
+if [ ! -d ./venv/ ]
+then
+    which virtualenv
+    if [[ $? -ne 0 ]]
+    then
+        pip install virtualenv
+    fi
+    virtualenv ./venv/
+fi
+
+function get_deps(){
+    echo $(grep "install_requires" setup.py | sed -n "s/install_requires =\[\(.*\)\]/\1/g;s/[\',]//g;p")
+}
+DEPS=($(get_deps))
+
+source ./venv/bin/activate
+pip install ${DEPS[@]}
+deactivate
