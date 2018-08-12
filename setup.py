b2998bf0e85d8cac383faee443dc626bfbcaa067
Matthijs Tadema <M.J.Tadema@gmail.com>
2018-08-12 13:52:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-    install_requires =['numpy', 'tk-tools', 'imutils', 'matplotlib', 'opencv-python', 'IPython'],
+    install_requires =['numpy', 'tk-tools', 'imutils', 'matplotlib', 'opencv-python', 'IPython', 'scipy'],
     scripts = [
         "bin/buffy_run.py"
     ],
